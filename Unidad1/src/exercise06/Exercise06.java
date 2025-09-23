5544662a8c8fc9cdfcd5034263191cdc0c17b185
qr.ye <qr.ye@106-30.info2>
2025-09-23 13:50:44 +0200

    intento 1

---
+++ Unidad1/src/exercise06/Exercise06.java
@@ -0,0 +1,25 @@
+package exercise06;
+
+import java.util.Scanner;
+
+public class Exercise06 {
+
+	public static void main(String[] args) { 
+		
+		//Primer número pedido al usuario
+		double numero1; 
+		
+		//Segundo número pedido al usuario
+		double numero2;
+		
+		//Creación del scanner
+		Scanner reader = new Scanner (System.in);
+		
+		//Pedir al usuario una cifra
+		System.out.println("Introduce un número");
+		numero1 = reader.nextDouble(); //aqui es donde el usuario lo verá
+		
+
+	}
+
+}
